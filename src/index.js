2cc9e0190ea2b799f6658412379d2f03695cbbf0
Ivan <ivan.s.palatov@gmail.com>
2020-05-31 20:45:59 +0400

    after homework #12 (Figma Counter)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const reducer = (state = 0, action) => {
+	switch (action.type) {
+		case 'INC':
+			return state + 1;
+		case 'DEC':
+			return state - 1;
+		case 'RND':
+			return state + action.value;
+		case 'RES':
+			return 0;
+
+		default:
+			return state;
+	}
+}
+
+let store = createStore(reducer);
+console.log(store.getState());
+
+const incAC = () => ({ type: 'INC' });
+const decAC = () => ({ type: 'DEC' });
+const rndAC = (value) => ({ type: 'RND', value })
+const resAC = () => ({ type: 'RES' })
+
+document.getElementById('inc').addEventListener('click', () => {
+	store.dispatch(incAC())
+})
+document.getElementById('dec').addEventListener('click', () => {
+	store.dispatch(decAC())
+})
+document.getElementById('res').addEventListener('click', () => {
+	store.dispatch(resAC())
+})
+
+const update = () => {
+	document.getElementById('counter').innerHTML = store.getState();
+}
+
+
+store.subscribe(update);
+
+// store.dispatch({ type: 'INC' });
+// store.dispatch({ type: 'INC' });
+// store.dispatch({ type: 'INC' });
